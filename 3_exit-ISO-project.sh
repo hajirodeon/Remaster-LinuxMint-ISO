be9793be89cd1323158244ce4e19fed3b9135beb
Meoso <meoso@users.noreply.github.com>
2016-08-12 09:10:54 -0500

    Update 3_exit-ISO-project.sh

--- 3_exit-ISO-project.sh
+++ 3_exit-ISO-project.sh
@@ -8,8 +8,14 @@ rm -rf /tmp/* ~/.bash_history ~/.nano_history
 umount /proc
 umount /sys
 umount /dev/pts
-echo "now you must:"
+
+red="\e[31m"
+normal="\e[0m"
+
+echo ""
+echo -e "${red} Now you must:"
 #echo "exit"
 #echo "sudo umount /edit/dev"
 #echo "sudo umount mnt"
-echo "sudo reboot"
+echo -e "${red} sudo reboot"
+echo -e "${nocolor}"
